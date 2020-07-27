0c34a0e5d4184d144f91081f4c4a8c3df26a413d
ismailpmi <mdismailpmi@gmail.com>
2020-07-27 13:05:01 +0530

    Added Schema and Model code

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/bookDB", { useNewUrlParser: true });
+
+const bookSchema = {
+    bookName: String,
+    authorName: String,
+    review: Number,
+};
+
+const Book = mongoose.model("Book", bookSchema);
+
+
+
 //server listerning port setting
 app.listen(3000, function() {
   console.log("Server started on port 3000");
